f4a07070266cc1e33608a088325a98b8909eb91a
farhad_davaripour <farhad.davaripour@gmail.com>
2025-04-05 05:25:36 -0600

    bug: revert back the changes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 from core.core import create_message_text, generate_response, process_and_plot
 
 # Define sliders in the sidebar
@@ -8,6 +7,7 @@ temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, val
 top_p = st.sidebar.slider("Top P", min_value=0.0, max_value=1.0, value=0.8, step=0.01)
 top_logprobs = st.sidebar.slider("Top Logprobs", min_value=1, max_value=10, value=5, step=1)
 n = st.sidebar.slider("Number of Completions (n)", min_value=1, max_value=10, value=1, step=1)
+
 
 # Create a function for generating and displaying responses
 def generate_and_display_responses(prompt):
@@ -21,11 +21,7 @@ def generate_and_display_responses(prompt):
 
     # Process and plot
     st.subheader("Response Plot")
-    if ax is None:
-        fig, ax = plt.subplots()
-    else:
-        fig = ax.figure
-    process_and_plot(response, ax=ax)
+    fig = process_and_plot(response)
     st.pyplot(fig)
 
 # Streamlit app layout
